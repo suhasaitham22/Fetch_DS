9dcee26f5f4b9dba0e8e787cf2af4a818e0ea0f2
Suhas Aitham <83656128+suhasaitham22@users.noreply.github.com>
2023-11-14 19:43:26 -0500

    Update fetch_search.py

--- fetch_search.py
+++ fetch_search.py
@@ -10,7 +10,8 @@ except AttributeError:
     pass
 else:
     ssl._create_default_https_context = _create_unverified_https_context
-    
+
+nltk.download()
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('stopwords')
