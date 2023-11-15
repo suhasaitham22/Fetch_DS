edbbefc077c1e493092099a0878ac0dc9a908112
Suhas Aitham <83656128+suhasaitham22@users.noreply.github.com>
2023-11-14 19:58:14 -0500

    Update fetch_search.py

--- fetch_search.py
+++ fetch_search.py
@@ -2,17 +2,6 @@ import streamlit as st
 import pandas as pd
 import string
 import nltk
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
