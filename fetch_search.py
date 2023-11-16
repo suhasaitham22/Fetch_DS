b70034a5cb5629411e46bf253660eff891e000d2
Suhas Aitham <83656128+suhasaitham22@users.noreply.github.com>
2023-11-16 02:35:55 -0500

    Update fetch_search.py

--- fetch_search.py
+++ fetch_search.py
@@ -208,6 +208,33 @@ def preprocess_text_fuzzy(text, target):
         return target
     return text
     
+def get_relevant_offers_for_retailer(user_input_retailer, data, tokenizer_offer, tokenizer_retailer, tokenizer_brand, model, scaler):
+    user_input_sequence_retailer = tokenizer_retailer.texts_to_sequences([user_input_retailer])
+    user_input_sequence_retailer = pad_sequences(user_input_sequence_retailer, maxlen=X_retailer.shape[1])
+
+    user_input_sequence_retailer_broadcasted = np.repeat(user_input_sequence_retailer, X_offer.shape[0], axis=0)
+
+    user_pred = model.predict([X_offer, user_input_sequence_retailer_broadcasted, X_brand])
+
+    user_pred_absolute = np.abs(user_pred)
+
+    user_pred_original = scaler.inverse_transform(user_pred_absolute).flatten()
+
+    result_df = pd.DataFrame({
+        'OFFER': data['OFFER'],
+        'RETAILER': data['RETAILER'],
+        'BRAND': data['BRAND'],
+        'PRODUCT_CATEGORY': data['PRODUCT_CATEGORY'],
+        'predicted_score': user_pred_original
+    })
+
+    result_df = result_df[result_df['RETAILER'].str.lower() == user_input_retailer.lower()]
+
+    result_df = result_df.sort_values(by='predicted_score', ascending=False)
+
+    result_df.reset_index(drop=True, inplace=True)
+
+    return result_df.head(10)   
 
 # Model Page
 if page == 'Model':
@@ -291,60 +318,29 @@ if page == 'Model':
             
         option = st.selectbox("Select option:", ('Brand', 'Category', 'Retailer'))
             
-        if option:
-            search_query = st.text_input(f"Enter {option} for search:")
-            
-            if search_query:
-                # Function to get relevant offers for a user input retailer
-                def get_relevant_offers_for_retailer(user_input_retailer, data, tokenizer_offer, tokenizer_retailer, tokenizer_brand, model, scaler):
-                    # Transform the user input using the tokenizer
-                    user_input_sequence_retailer = tokenizer_retailer.texts_to_sequences([user_input_retailer])
-                    user_input_sequence_retailer = pad_sequences(user_input_sequence_retailer, maxlen=tokenizer_retailer.document_count)
-                
-                    # Repeat the user input sequence for each offer
-                    user_input_sequence_retailer_broadcasted = np.repeat(user_input_sequence_retailer, tokenizer_offer.document_count, axis=0)
-                
-                    # Predict the similarity scores for the user input
-                    user_pred = model.predict([X_offer, user_input_sequence_retailer_broadcasted, X_brand])
-                
-                    # Take the absolute value of the predicted scores
-                    user_pred_absolute = np.abs(user_pred)
-                
-                    # Inverse transform to get the original scale
-                    user_pred_original = scaler.inverse_transform(user_pred_absolute).flatten()
-                    
-                    # Call the function with all required arguments
-                    result_df = get_relevant_offers_for_retailer(
-                        search_query.lower(),
-                        data,  # Pass the 'data' variable
-                        tokenizer_offer,  # Pass the 'tokenizer_offer' variable
-                        tokenizer_retailer,  # Pass the 'tokenizer_retailer' variable
-                        tokenizer_brand,  # Pass the 'tokenizer_brand' variable
-                        model,  # Pass the 'model' variable
-                        scaler  # Pass the 'scaler' variable
-                    )
-                    # Filter offers for the specified retailer
-                    result_df = result_df[result_df['RETAILER'].str.lower() == user_input_retailer.lower()]
-                
-                    # Sort offers based on predicted score
-                    result_df = result_df.sort_values(by='predicted_score', ascending=False)
-                
-                    # Reset index
-                    result_df.reset_index(drop=True, inplace=True)
-                
-                    return result_df.head(10) 
-    
-                result_df = get_relevant_offers_for_retailer(search_query.lower())
-    
+        if option and (search_query := st.text_input(f"Enter {option} for search:")):
+            result_df = get_relevant_offers_for_retailer(
+                search_query.lower(),
+                data,
+                tokenizer_offer,
+                tokenizer_retailer,
+                tokenizer_brand,
+                model,
+                scaler
+            )
+        
+            if result_df is not None:
                 if not result_df.empty:
                     st.header('Top Similar Offers:')
                     st.dataframe(result_df[['BRAND', 'OFFER', 'predicted_score']])
                 else:
                     st.info(f"No offers found for the given search query: '{search_query}'.")
-    
+        
                 check_future_offers = st.checkbox("Check for offers in the future")
-    
+        
                 if check_future_offers:
                     st.info("Checking for future offers... (This functionality is not yet implemented)")
             else:
                 st.info("Enter a search query to view results.")
+        else:
+            st.info("Enter a search query to view results.")
